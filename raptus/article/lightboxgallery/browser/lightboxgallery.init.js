952a94b7aad9b27b9c49c8f2acf68340eb6474e8
Simon Käser <skaeser@raptus.com>
2013-07-01 16:50:53 +0200

    Only show hidden images when in editing mode (introduced in raptus.article.core 2.0b13), fixed template to no longer render an html tag and adjusted javascript initialization to work with the new component selection functionality introduced in raptus.article.core 2.0b13

--- raptus/article/lightboxgallery/browser/lightboxgallery.init.js
+++ raptus/article/lightboxgallery/browser/lightboxgallery.init.js
@@ -1,13 +1,22 @@
-jQuery(document).ready(function() {
-  var images = new Array();
-  var settings = inlinelightbox.settings['raptus_article_lightboxgallery'];
-  jQuery('a[rel^=lightboxgallery]').each(function() {
-    var o = jQuery(this);
-    var rel = o.attr('rel');
-    if(jQuery.inArray(rel, images) == -1)
-      images.push(rel);
-  });
-  for(var i=0; i<images.length; i++) {
-    jQuery('a[rel="'+images[i]+'"]').inlineLightBox(settings);
+(function($) {
+
+  function init(e) {
+    var images = new Array();
+    var settings = inlinelightbox.settings['raptus_article_lightboxgallery'];
+    $(this).find('a[rel^=lightboxgallery]').each(function() {
+      var o = $(this);
+      var rel = o.attr('rel');
+      if($.inArray(rel, images) == -1)
+        images.push(rel);
+    });
+    for(var i=0; i<images.length; i++) {
+      $(this).find('a[rel="'+images[i]+'"]').inlineLightBox(settings);
+    }
   }
-});
+
+  $(document).ready(function(e) {
+    $.proxy(init, $('body'))(e);
+    $('.viewletmanager').on('viewlets.updated', init);
+  });
+
+})(jQuery);
--- raptus/article/lightboxgallery/browser/lightboxgallery.py
+++ raptus/article/lightboxgallery/browser/lightboxgallery.py
@@ -16,24 +16,27 @@ from raptus.article.core import RaptusArticleMessageFactory as _
 from raptus.article.core import interfaces
 from raptus.article.images.interfaces import IImages, IImage
 
+
 class ILightboxGallery(interface.Interface):
     """ Marker interface for the lightbox gallery viewlet
     """
+
 
 class Component(object):
     """ Component which shows a lightbox gallery of the images
     """
     interface.implements(interfaces.IComponent, interfaces.IComponentSelection)
     component.adapts(interfaces.IArticle)
-    
+
     title = _(u'Lightbox with gallery')
     description = _(u'Lightbox of the images contained in the article including a horizontal gallery.')
     image = '++resource++lightboxgallery.gif'
     interface = ILightboxGallery
     viewlet = 'raptus.article.lightboxgallery'
-    
+
     def __init__(self, context):
         self.context = context
+
 
 class Viewlet(ViewletBase):
     """ Viewlet showing the lightbox gallery of the images
@@ -43,14 +46,14 @@ class Viewlet(ViewletBase):
     thumb_size = "lightboxgallerythumb"
     img_size = "lightboxgallery"
     component = "lightboxgallery"
-    
+
     @property
     @memoize
     def images(self):
         provider = IImages(self.context)
         manageable = interfaces.IManageable(self.context)
         mship = getToolByName(self.context, 'portal_membership')
-        if mship.checkPermission(MANAGE_PERMISSION, self.context):
+        if interfaces.IArticleEditView.providedBy(self.view) and mship.checkPermission(MANAGE_PERMISSION, self.context):
             items = provider.getImages()
         else:
             items = provider.getImages(component=self.component)
